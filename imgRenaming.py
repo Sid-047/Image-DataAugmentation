d577a3b368f58d5e1f49664707d0179ec42155df
Sid <siddiqmoideen07@gmail.com>
2023-12-06 15:14:58 +0530

    Updated imgReNaming Stuff

---
+++ imgRenaming.py
@@ -0,0 +1,20 @@
+from tkinter import filedialog
+import random
+import tqdm
+import glob
+import time
+import os
+
+t1 = time.time()
+print("Select the Directory Yo!")
+imgDir = filedialog.askdirectory() + '\\'
+print("---->", imgDir)
+x = [glob.glob(imgDir+y) for y in ['*.jpg', '*.png', '*.tiff', '*.bmp', '*.jpeg']]
+random.shuffle(x)
+
+a=0
+for i in tqdm(x, desc = "ReNamin' the Images Yo!", color = 'red'):
+    os.rename(i, imgDir+str(a)+'.png')
+    a+=1
+t2 = time.time()
+print("\nCompleteExecTime: ", (t2-t1))
