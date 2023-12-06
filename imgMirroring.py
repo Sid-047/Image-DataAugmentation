d5af197690d3b907bb3882c5bcdedf8858cceb02
Sid <sid@adoltech.com>
2023-12-06 14:46:53 +0530

    Minor Updates

--- imgMirroring.py
+++ imgMirroring.py
@@ -10,8 +10,8 @@ print("Select the Directory Yo!")
 imgDir = filedialog.askdirectory() + '\\'
 print("---->", imgDir)
 x = [glob.glob(imgDir+y) for y in ['*.jpg', '*.png', '*.tiff', '*.bmp', '*.jpeg']]
-
-for i in tqdm(x, desc = "Mirrorin' the Images Yo!"):
+x = sum(x, [])
+for i in tqdm.tqdm(x, desc = "Mirrorin' the Images Yo!"):
     t1_ = time.time()
     img = Image.open(i).convert('RGB')
     imgAr = np.array(img)
