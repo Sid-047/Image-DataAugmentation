c63f68a5180cdaf7d1f452367cb5d29cc7d233bd
Sid <siddiqmoideen07@gmail.com>
2024-03-19 17:07:04 +0530

    imgInversion Convo stuff

---
+++ imgInvert.py
@@ -0,0 +1,26 @@
+from tkinter import filedialog
+from PIL import Image
+import numpy as np
+import tqdm
+import glob
+import time
+
+t1 = time.time()
+print("Select the Directory Yo!")
+imgDir = filedialog.askdirectory() + '\\'
+print("---->", imgDir)
+x = [glob.glob(imgDir+y) for y in ['*.jpg', '*.png', '*.tiff', '*.bmp', '*.jpeg']]
+x = sum(x , [])
+for i in tqdm.tqdm(x, desc = "Mirrorin' the Images Yo!", colour = 'red'):
+    t1_ = time.time()
+    img = Image.open(i).convert('RGB')
+    imgAr = np.array(img)
+    mirImg = np.flipud(imgAr)
+    outImg = Image.fromarray(mirImg)
+
+    z = i.split(".")
+    z = str('_.').join(z)
+    outImg.save(z)
+    t2_ = time.time()
+t2 = time.time()
+print("\nCompleteExecTime: ", (t2-t1))
